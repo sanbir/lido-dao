a3a54f9a33ea06b3c07ea353a3c5f1d0b4351ca6
Alexander Biryukov <alexander.biryukov@p2p.org>
2022-07-20 11:47:41 +0300

    submitGNOWithPermit

--- contracts/0.4.24/LidoOnGnosis.sol
+++ contracts/0.4.24/LidoOnGnosis.sol
@@ -40,7 +40,8 @@ contract LidoOnGnosis {
     }
 
     /**
-    * @notice Send mGNO to the pool with optional _referral parameter
+    * @dev Send mGNO to the pool with optional _referral parameter.
+    * @notice Requires user approval.
     * @param _amount amount of mGNO.
     * @param _referral address of referral.
     * @return Amount of StETH shares generated
@@ -60,7 +61,8 @@ contract LidoOnGnosis {
         address from,
         uint256 value,
         bytes data
-    ) external returns (bool) {
+    ) external returns (bool)
+    {
         address token = msg.sender;
         require(token == address(getMGNO()), "mGNO only");
 
@@ -70,15 +72,55 @@ contract LidoOnGnosis {
     }
 
     /**
-    * @notice Send GNO to the pool with optional _referral parameter
+    * @dev Send GNO to the pool with optional _referral parameter.
+    * @notice Requires user approval.
     * @param _amount amount of GNO.
     * @param _referral address of referral.
     * @return Amount of StETH shares generated
     */
-    function submitGNO(uint256 _amount, address _referral) external returns (uint256) {
+    function submitGNO(uint256 _amount, address _referral) public returns (uint256) {
         _receiveGno(_amount);
         _convertGnoToMgno(_amount);
         return _submit(_amount.mul(32), _referral, msg.sender); // 1 GNO = 32 mGNO
+    }
+
+    /**
+    * @dev Send GNO to the pool with optional _referral parameter.
+    * @notice Does not require user approval. Accepts offline signature instead.
+    * @param _amount amount of GNO.
+    * @param _referral address of referral.
+    * @param _nonce The nonce taken from `nonces(_holder)` public getter.
+    * @param _expiry The allowance expiration date (unix timestamp in UTC).
+    * Can be zero for no expiration. Forced to zero if `_allowed` is `false`.
+    * Note that timestamps are not precise, malicious miner/validator can manipulate them to some extend.
+    * Assume that there can be a 900 seconds time delta between the desired timestamp and the actual expiration.
+    * @param _v A final byte of signature (ECDSA component).
+    * @param _r The first 32 bytes of signature (ECDSA component).
+    * @param _s The second 32 bytes of signature (ECDSA component).
+    * @return Amount of StETH shares generated
+    */
+    function submitGNOWithPermit(
+        uint256 _amount,
+        address _referral,
+        uint256 _nonce,
+        uint256 _expiry,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    ) external override
+    {
+        getGNO().permit(
+            msg.sender,
+            address(this),
+            _nonce,
+            _expiry,
+            true,
+            _v,
+            _r,
+            _s
+        );
+
+        return submitGNO(_amount, _referral);
     }
 
     function _receiveMgno(uint256 _amount) internal {
